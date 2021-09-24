24cc7fa282e3942ee287deae6f6ee37515ab2f65
David Hong <phazex1@gmail.com>
2021-09-24 12:18:11 -0400

    Adding Arrays answers

--- arrays.js
+++ arrays.js
@@ -4,11 +4,13 @@
 const faveColors = ['red', 'green', 'black']
 
 //CODE HERE
+const colorCopy = faveColors.slice()
 
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
+faveColors.push ('blue')
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -19,6 +21,8 @@ const faveColors = ['red', 'green', 'black']
 const numbers = [1, 2, 3, 4, 5]
 
 //CODE HERE
+const middleNums = numbers.splice(1, 3)
+console.log (middleNums)
 
 
 //////////////////PROBLEM 4////////////////////
@@ -35,3 +39,15 @@ const numbers = [1, 2, 3, 4, 5]
 */
 
 // CODE HERE
+
+function bigOrSmall (arr) {
+  const answers = []
+  for (let i = 0; i < arr.length; i++){
+    if (arr[i] > 100){
+      answers.push('big')
+    } else {
+      answers.push('small')
+    }
+  }
+  return answers
+}
